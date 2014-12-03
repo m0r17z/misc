9646daee1e484cf353756efead2360c291e55797
Moritz August <moritz.august@tum.de>
2014-12-03 16:20:51 +0100

    bugfix

--- deep_learning/generate_eigset.py
+++ deep_learning/generate_eigset.py
@@ -7,9 +7,9 @@ num_samples = 100000
 dimensions = 100
 
 f = h5.File('eigdata.hdf5','w')
-matset = f.create_dataset("matrices", (num_samples, dimensions**2), dtype='c')
+matset = f.create_dataset("matrices", (num_samples, dimensions**2), dtype='complex64')
 eigvalset = f.create_dataset("eigvals", (num_samples, 1), dtype='f')
-eigvecset = f.create_dataset("eigvecs", (num_samples, dimensions), dtype='c')
+eigvecset = f.create_dataset("eigvecs", (num_samples, dimensions), dtype='complex64')
 
 
 for i in np.arange(num_samples):
